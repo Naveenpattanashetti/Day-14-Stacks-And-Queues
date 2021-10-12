f6330ed0f898084b49d08137c4ea1b726cf6d60f
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-12 21:03:32 +0530

    Added : Program for the Create the Stack

--- src/com/bridgelabz/DataStructure_Stack_Queue/StackQueqeMain.java
+++ src/com/bridgelabz/DataStructure_Stack_Queue/StackQueqeMain.java
@@ -7,7 +7,7 @@ public class StackQueqeMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the data Structure in 'Stack and Queqe' Program!!");
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Press '1' to add element at the start  ");
+		System.out.println("Press '1' to add element at the start");
 		switch (sc.nextInt()) {
 		case 1:
 			Operation.addElement();
